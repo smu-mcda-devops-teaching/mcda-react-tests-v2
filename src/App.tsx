d9b87a6e2cbef778e43677699571015e6446377c
Gbenro Selere <seleregb@gmail.com>
2022-11-07 16:21:33 -0400

    changed app title

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>This is Prod</p>
+        <p>This is QA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
